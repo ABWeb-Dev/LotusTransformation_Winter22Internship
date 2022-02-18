7e382cc6696b9c76f3a5ba2c201182913167ea0d
fabricekalvanda <fabricekalvanda@gmail.com>
2022-02-17 23:51:29 -0800

    Creates Migration

--- LotusTransformation/Models/LogIn.cs
+++ LotusTransformation/Models/LogIn.cs
@@ -19,9 +19,6 @@ namespace LotusTransformation.Models
         [Key()]
         public long UserID{ get; set; }
         public string UserName { get; set; }
-
-        public string Email { get; set; }
-
         public string Password { get; set; }
 
         public virtual UserInformation UserInformation { get; set; }
