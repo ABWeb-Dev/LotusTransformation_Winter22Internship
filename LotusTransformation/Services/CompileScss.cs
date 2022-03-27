f45486b5b5b1df03220a05160526f47bc1f6fee5
ABWeb-Dev <abradford.webdev@gmail.com>
2022-03-27 16:49:43 -0700

    Revised, Removed Bootstrap, other packages and frameworks. Using Scss, JQuery and MediaQueries for responsive design. Easier than altering bootstrap's scss files (seems to cause errors after several changes)

---
+++ LotusTransformation/Services/CompileScss.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+//figure out how to import LibSass
+namespace LotusTransformation.Services
+{
+    public static class CompileScss
+    {
+        public static string pathToScss = @"C:\Users\natsu\Music\OneDrive\Desktop\Bethany's Site\LotusTransformation\LotusTransformation\wwwroot\CustomScss\LayoutCustom.scss";
+        public static string pathToCss = @"C:\Users\natsu\Music\OneDrive\Desktop\Bethany's Site\LotusTransformation\LotusTransformation\wwwroot\CustomCss\CSSReset.css";
+
+        public static string CompiledFile()
+        {
+
+            TextReader reader = new StreamReader(pathToScss);
+
+            string CompiledCss = reader.ReadToEnd();
+
+            //CompiledCss
+
+            return CompiledCss;
+        }
+
+
+    }
+}
